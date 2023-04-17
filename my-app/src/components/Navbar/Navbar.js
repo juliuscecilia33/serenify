d6b9e9b463f99e72c57677a4f08d0a3dc6ec056b
Julius <juliuscecilia33@gmail.com>
2023-04-16 18:23:34 -0700

    navbar

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import biglogo from "../../images/biglogo.png";
+import logout from "../../images/logout.png";
 
 export function Navbar() {
   return (
     <div class="navbar-container">
       <img class="logo-image" src={biglogo} alt="Big Logo" />
+      <button>
+        <img class="logout-image" src={logout} alt="Logout" />
+      </button>
     </div>
   );
 }
