be654ded267663037a7777e07faca348f12ef5b3
Dongchunhua1225 <hdong25@uw.edu>
2023-04-18 19:56:50 -0700

    add get/delete, and check user exist query inall routes

--- server/routers/users.js
+++ server/routers/users.js
@@ -17,6 +17,16 @@ router.post("/register", async (req, res) => {
   try {
     const { useremail, userpassword, logintime } = req.body;
 
+    //check whether useremail has already been registered
+    const user = await pool.query(
+      "SELECT * FROM tbluser WHERE userEmail = $1",
+      [useremail]
+    );
+
+    if (user.rows.length !== 0) {
+      return res.status(401).json("User has already existed...");
+    }
+
     let newUser = await pool.query(
       "INSERT INTO tbluser (userEmail, userName, userPassword, loginTime, portrait, blockList, blockedList, reportCount) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
       [useremail, userpassword, logintime]
@@ -61,6 +71,7 @@ router.post("/login", async (req, res) => {
 });
 
 //Put the LoginTime
+//add a check
 router.put("/logintime", async (req, res) => {
   const { useremail } = req.body;
   //generate the current timestamp
@@ -69,6 +80,17 @@ router.put("/logintime", async (req, res) => {
   //console.log("Date Object:", dateObject);
 
   try {
+    //check whether user exist
+    const user = await pool.query(
+      "SELECT * FROM tbluser WHERE userEmail = $1",
+      [useremail]
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(401).json("User does not exist...");
+    }
+    
+    //put in the new login time
     const time = await pool.query(
       "UPDATE tbluser SET logintime = $1 WHERE userEmail = $2",
       [dateObject, useremail]
@@ -76,6 +98,62 @@ router.put("/logintime", async (req, res) => {
 
     res.json("Successfully updated time to " + dateObject);
     // res.json("Sucessfully update the logintime");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//get user info by useremail
+router.get("/:useremail", async(req, res) => {
+  try{
+    const { useremail } = req.params;
+
+    //check whether user exist
+    const user = await pool.query(
+      "SELECT * FROM tbluser WHERE userEmail = $1",
+      [useremail]
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(401).json("User does not exist...");
+    }
+
+    //get the user info
+    const getUserInfo = await pool.query(
+      "SELECT * FROM tbluser WHERE useremail = $1",
+      [useremail]
+    );
+
+    res.json(getUserInfo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//Delete a user by useremail
+router.delete("/:useremail", async(req, res) => {
+  try {
+    const { useremail } = req.params;
+
+    //check whether user exist
+    const user = await pool.query(
+      "SELECT * FROM tbluser WHERE userEmail = $1",
+      [useremail]
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(401).json("User does not exist...");
+    }
+    
+    //if user exists, delete the user and return the email
+    const deleteUser = await pool.query(
+      "DELETE FROM tblUser WHERE userid = $1",
+      [useremail]
+    );
+
+    res.json("Successfully Delete the user: " + useremail);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
