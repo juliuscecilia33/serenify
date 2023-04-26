aa565fb4e11622fd2edbd42f47de28607936b44f
xiaowh3 <xiaowh3@uw.edu>
2023-04-25 17:15:02 -0700

    yellow design 1/2 x1.1

--- my-app/src/pages/HomeVTwo/HomeVTwo.js
+++ my-app/src/pages/HomeVTwo/HomeVTwo.js
@@ -3,102 +3,97 @@ import "./HomeVTwo.css";
 
 export function HomeVTwo() {
   return (
-    <div className="container">
-    <div className="home-v-two-home-v-two">
-      <div className="home-v-two-content">
+    <div className="content">
+      <img
+        className="logo"
+        alt={"Logo"}
+        src={"../../src/images/logo2.png"}
+      />
+      <div className="log-in">Log in</div>
+      <div className="s1">
+        <h1 className="big">
+          Hello
+          <br />
+          &lt;･◡･&gt;!
+          <br />
+          <br />I am Serenify.
+          <br />
+          ---------
+        </h1>
+      </div>
+      <div className="s2">
+        <h1 className="big">
+          Your 
+        </h1>
+        <p className="small smallcross">
+          &nbsp;Online&nbsp;
+          <br /> 
+          &nbsp;Anonymous&nbsp;
+          <br /> 
+          &nbsp;Mental Health&nbsp;
+          <br /> 
+          &nbsp;Prompt Based&nbsp;
+          <br />
+        </p>
+        <h1 className="big">
+          Forum.
+          <br />
+          ---------
+        </h1>
+      </div>
+      <div className="s3">
+        <p className="small">
+          &nbsp;I like to
+        </p>
+        <h1 className="big">
+          Scroll
+        </h1>
+        <p className="small">
+          &nbsp;and
+        </p>
+        <h1 className="big">
+          Hold
+        </h1>
+        <span className="small">
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Try it~^
+          <br />
+        </span>
+        <h1 className="big">
+          ---------
+        </h1>
         <img
-          className="home-v-two-logo"
-          alt={"Logo"}
+          className="wheel"
+          alt={"Wheel"}
           src={
-            "https://anima-uploads.s3.amazonaws.com/projects/64480ae0ca3bfce771e46398/releases/64482982ca3bfce771e4641d/img/logo.svg"
-        }
+            "https://anima-uploads.s3.amazonaws.com/projects/64480ae0ca3bfce771e46398/releases/6448516f8ae30cff1df92e11/img/wheel.svg"
+          }
         />
-        <div className="home-v-two-text-wrapper">Log in</div>
-        <div className="home-v-two-b">
-          <h1 className="home-v-two-hello-i-am-serenify">
-            Hello
-            <br />
-            &lt;･◡･&gt;!
-            <br />
-            <br />I am Serenify.
-            <br />
+      </div>
+      <div className="s3">
+        <p className="small">
+          &nbsp;You can
+          <br /> 
+          &nbsp;Pet this cat 
+          <br /> 
+          &nbsp;To move forward.
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <h1 className="big">
             ---------
           </h1>
-        </div>
-        <div className="home-v-two-p-wrapper">
-          <p className="home-v-two-your-online-anonymous-mental-health-prompt-based-forum">
-            <span className="home-v-two-span">
-              Your
-              <br />
-            </span>
-            <span className="home-v-two-text-wrapper-2">
-              &nbsp;Onlin&nbsp; 
-              <br /> 
-              &nbsp;Anonymous&nbsp; 
-              <br /> 
-              &nbsp;Mental Health&nbsp;
-              <br /> 
-              &nbsp;Prompt Based&nbsp;
-              <br />
-            </span>
-            <span className="home-v-two-span">
-              Forum.
-              <br />
-              ---------
-            </span>
-          </p>
-        </div>
-        <div className="home-v-two-p-wrapper">
-          <p className="home-v-two-p">
-            <span className="home-v-two-text-wrapper-3">
-              &nbsp;I like to
-              <br />
-            </span>
-            <span className="home-v-two-text-wrapper-4">
-              Scroll
-              <br />
-            </span>
-            <span className="home-v-two-text-wrapper-3">
-              &nbsp;And
-              <br />
-            </span>
-            <span className="home-v-two-text-wrapper-4">
-              Hold&nbsp;※<br/>
-            </span>
-            <span className="home-v-two-text-wrapper-6">
-              <br />
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Try it~^
-              <br />
-            </span>
-            <span className="home-v-two-text-wrapper-4">---------</span>
-          </p>
-        </div>
-        <div className="home-v-two-p-wrapper">
-          <p className="home-v-two-p">
-            <span className="home-v-two-text-wrapper-6">&nbsp;</span>
-            <span className="home-v-two-text-wrapper-3">
-              You can
-              <br /> &nbsp;Pet this cat <br /> &nbsp;To move forward.
-              <br />
-            </span>
-            <span className="home-v-two-text-wrapper-6">
-              <br />
-            </span>
-            <span className="home-v-two-text-wrapper-7">
-              ㅤ／l、
-              <br />
-              （ﾟ､ ｡７
-              <br />
-              ㅤl、~ヽ
-              <br />
-              &nbsp;&nbsp;&nbsp;じしf_,)ノ
-              <br />
-            </span>
-            <span className="home-v-two-text-wrapper-4">---------</span>
-          </p>
-        </div>
+        </p>
+        <img
+          className="cat"
+          alt={"Cat"}
+          src={
+            "https://anima-uploads.s3.amazonaws.com/projects/64480ae0ca3bfce771e46398/releases/6448516f8ae30cff1df92e11/img/cat.svg"
+          }
+        />
       </div>
-    </div>
     </div>
   );
 }
