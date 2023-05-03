095be06b340856ecfb7772e3fea1c8edfac22775
Julius <juliuscecilia33@gmail.com>
2023-05-02 17:43:17 -0700

    report

--- my-app/src/pages/Report/Report.js
+++ my-app/src/pages/Report/Report.js
@@ -3,6 +3,12 @@ import * as ROUTES from "../../constants/routes";
 import "./Report.css";
 
 export function Report() {
+  const [optionOneSelected, setOptionOneSelected] = useState(false);
+  const [optionTwoSelected, setOptionTwoSelected] = useState(false);
+  const [optionThreeSelected, setOptionThreeSelected] = useState(false);
+  const [optionFourSelected, setOptionFourSelected] = useState(false);
+  const [optionFiveSelected, setOptionFiveSelected] = useState(false);
+
   return (
     <div className="report-report">
       <div className="report-content">
@@ -18,12 +24,67 @@ export function Report() {
           <h1 className="report-text-wrapper">Report?</h1>
         </div>
         <div className="report-b-2">
-          <div className="report-false-information">&gt; False information</div>
-          <div className="report-div">Harassment</div>
-          <div className="report-div">Hate speech</div>
-          <div className="report-div">Spam</div>
-          <div className="report-div">Other...</div>
-          <div className="report-text-wrapper-2">-&gt;</div>
+          <button
+            onClick={() => {
+              setOptionOneSelected(true);
+              setOptionTwoSelected(false);
+              setOptionThreeSelected(false);
+              setOptionFourSelected(false);
+              setOptionFiveSelected(false);
+            }}
+            className={optionOneSelected ? "report-div-bold" : "report-div"}
+          >
+            {optionOneSelected && ">"} False information
+          </button>
+          <button
+            onClick={() => {
+              setOptionOneSelected(false);
+              setOptionTwoSelected(true);
+              setOptionThreeSelected(false);
+              setOptionFourSelected(false);
+              setOptionFiveSelected(false);
+            }}
+            className={optionTwoSelected ? "report-div-bold" : "report-div"}
+          >
+            {optionTwoSelected && ">"} Harassment
+          </button>
+          <button
+            onClick={() => {
+              setOptionOneSelected(false);
+              setOptionTwoSelected(false);
+              setOptionThreeSelected(true);
+              setOptionFourSelected(false);
+              setOptionFiveSelected(false);
+            }}
+            className={optionThreeSelected ? "report-div-bold" : "report-div"}
+          >
+            {optionThreeSelected && ">"} Hate speech
+          </button>
+          <button
+            onClick={() => {
+              setOptionOneSelected(false);
+              setOptionTwoSelected(false);
+              setOptionThreeSelected(false);
+              setOptionFourSelected(true);
+              setOptionFiveSelected(false);
+            }}
+            className={optionFourSelected ? "report-div-bold" : "report-div"}
+          >
+            {optionFourSelected && ">"} Spam
+          </button>
+          <button
+            onClick={() => {
+              setOptionOneSelected(false);
+              setOptionTwoSelected(false);
+              setOptionThreeSelected(false);
+              setOptionFourSelected(false);
+              setOptionFiveSelected(true);
+            }}
+            className={optionFiveSelected ? "report-div-bold" : "report-div"}
+          >
+            {optionFiveSelected && ">"} Other...
+          </button>
+          <button className="report-text-wrapper-2">-&gt;</button>
         </div>
       </div>
     </div>
